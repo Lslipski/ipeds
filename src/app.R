672587d22f59393028a12e707a24a045ebf15134
lslipski <lslipski602@gmail.com>
2025-08-16 21:52:02 -0700

    initial commit app with primary reserve ratio table

---
+++ src/app.R
@@ -0,0 +1,162 @@
+library(shiny)
+library(tidyverse)
+library(janitor)
+library(bslib)
+library(gt)
+source(here::here('src/functions/load_functions.R'))
+load_functions()
+
+
+# Load data --------------------------------------------------------------------
+
+years = c("2017",
+          "2018",
+          "2019",
+          "2020",
+          "2021",
+          "2022", 
+          "2023")
+
+
+df_f1a <- load_public_financial(years)
+df_directory <- load_directory(years) %>% 
+  mutate_if(is.character, utf8::utf8_encode)
+
+
+
+# Define UI --------------------------------------------------------------------
+
+ui <- page_sidebar(
+  
+  
+  sidebar = sidebar(
+    
+    selectInput(inputId = "this_year", 
+                label = "Year",
+                choices = years,
+                selected = "2023",
+                multiple = TRUE),
+    
+    # Choose University
+    selectInput(inputId = "this_college",
+                label = "University",
+                choices = df_directory %>% 
+                  distinct(instnm),
+                multiple = FALSE,
+                selected = )
+  ),
+  
+  card(
+    gt_output('tbl_primary_reserve')
+  )
+)
+
+
+
+# Define server ----------------------------------------------------------------
+
+server <- function(input, output, session) {
+  
+# create reactive data frame
+  df <- reactive({
+    req(input$this_college)
+    req(input$this_year)
+    df_f1a %>% 
+      dplyr::left_join(df_directory,
+                       by = c("unitid" = "unitid",
+                              "year" = "year")) %>% 
+      dplyr::filter(instnm == input$this_college,
+                    year %in% input$this_year) %>% 
+      dplyr::select(year,
+                    unitid,
+                    instnm,
+                    expendable_net_assets =f1a17,
+                    total_expenses = f1d02,
+                    change_in_net_assets = f1d03,
+                    total_net_asssets = f1a14,
+                    op_inc_b09 = f1b09,
+                    op_inc_c110 = f1c101,
+                    revenue = f1b25,
+                    expendable_net_assets = f1a17,
+                    long_term_debt = f1a10) %>%
+      dplyr::mutate(primary_reserve_ratio = round(expendable_net_assets / total_expenses, 2),
+                    net_assets_ratio = round(change_in_net_assets / total_net_asssets, 2),
+                    operating_income = op_inc_b09 - op_inc_c110,
+                    net_operating_revenue_ratio = round(operating_income / revenue, 2),
+                    viability_ratio = round(expendable_net_assets / long_term_debt, 2)) %>%
+      dplyr::select(-unitid,
+                    -instnm)
+    
+  })
+  
+  output$tbl_primary_reserve <- render_gt({
+    df() %>% 
+      dplyr::select(year,
+                    expendable_net_assets,
+                    total_expenses,
+                    primary_reserve_ratio) %>% 
+      tidyr::pivot_longer(names_to = "column",
+                          values_to = "values",
+                          cols = -c("year"),
+                          values_transform = list(values = as.character)) %>% 
+      dplyr::mutate(values = as.numeric(values)) %>% 
+      dplyr::arrange(desc(year)) %>% 
+      tidyr::pivot_wider(names_from = "year",
+                  values_from = "values") %>% 
+      gt(
+        rowname_col = "column"
+      ) %>% 
+      tab_header(title = unis) %>% 
+      tab_spanner(
+        label = "Year",
+        columns = sort(input$this_year, decreasing = T)
+      ) %>%
+      fmt_number(
+        columns = starts_with("2"),
+        rows = c("expendable_net_assets",
+                 "total_expenses"),
+        decimals = 0,
+        use_seps = TRUE
+      ) %>% 
+      data_color(
+        columns = everything(),
+        rows = "primary_reserve_ratio",
+        palette = c("#EA8D2D")
+      )
+  })
+  
+}
+
+# Create the Shiny app object --------------------------------------------------
+
+shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
