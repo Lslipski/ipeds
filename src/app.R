676b67cdf0a4c36a7d6bf970ae378624e0b42501
lslipski <lslipski602@gmail.com>
2026-03-19 20:00:12 -0700

    initial commit on ratios after olivia provided new excel book with the correct variables and ratio descriptions

--- src/app.R
+++ src/app.R
@@ -9,22 +9,19 @@ load_functions()
 
 # Setup --------------------------------------------------------------------
 
-years = c("2017",
-          "2018",
-          "2019",
-          "2020",
+years = c("2020",
           "2021",
           "2022", 
           "2023")
 
 institution_types = c("Public",
-                      "Private",
+                      #"Private",
                       "Not-for-Profit")
 
 # data
 df_f1a <- load_public_financial(years)
 df_f2 <- load_nfp_financial(years)
-df_f3 <- load_private_financial(years)
+#df_f3 <- load_private_financial(years)
 df_directory <- load_directory(years) %>% 
   mutate_if(is.character, utf8::utf8_encode)
 df_institutions <- load_institution_types(years)
@@ -76,8 +73,7 @@ ui <- page_sidebar(
           card_header(
             "Primary Reserve Ratio"
           ),
-          card_body("The primary reserve ratio divides the expendable net assets by the total expenses of the university and 
-                    gives an estimate of..."),
+          card_body("The primary reserve ratio answers the question: Are resources sufficient and flexible enough to support the mission?"),
           gt_output('tbl_primary_reserve')
         ),
         
@@ -101,15 +97,18 @@ ui <- page_sidebar(
         #   gt_output('tbl_net_op_rev')
         # ),
         # 
-        # card(
-        #   min_height = min_card_height,
-        #   card_header(
-        #     "Viability Ratio"
-        #   ),
-        #   card_body("The viability ratio divides the expendable net assets by the long term debt of the university and 
-        #             gives an estimate of..."),
-        #   gt_output('tbl_viability')
-        # )
+        card(
+          min_height = min_card_height,
+          card_header(
+            "Viability Ratio"
+          ),
+          card_body(markdown("The viability ratio answers the questions: 
+          
+          1. Are debt resources managed strategically to advance the mission?
+          
+          2. Can the institution use its expendable net assets to cover debt?")),
+          gt_output('tbl_viability')
+        )
     ), # close nav_panel key ratios
     
     nav_panel(
@@ -183,19 +182,16 @@ server <- function(input, output, session) {
           dplyr::select(year,
                         unitid,
                         instnm,
-                        expendable_net_assets =f1a17,
+                        expendable_net_assets = f1n05,
                         total_expenses = f1d02,
-                        change_in_net_assets = f1d03,
-                        total_net_asssets = f1a14,
-                        op_inc_b09 = f1b09,
-                        op_inc_c110 = f1c101,
-                        revenue = f1b25,
-                        long_term_debt = f1a10) %>%
+                        plant_related_debt = f1d03
+                        #change_in_net_assets = 
+                        ) %>%
           dplyr::mutate(primary_reserve_ratio = round(expendable_net_assets / total_expenses, 2),
-                        net_assets_ratio = round(change_in_net_assets / total_net_asssets, 2),
-                        operating_income = op_inc_b09 - op_inc_c110,
-                        net_operating_revenue_ratio = round(operating_income / revenue, 2),
-                        viability_ratio = round(expendable_net_assets / long_term_debt, 2)) %>%
+                        viability_ratio = round(expendable_net_assets / plant_related_debt, 2)
+                        #return_on_net_assets_ratio = ,
+                        #net_operating_revenues_ratio = ,
+                        ) %>%
           dplyr::select(-unitid,
                         -instnm)}
       else if (input$this_type == "Private") {
@@ -227,17 +223,14 @@ server <- function(input, output, session) {
           dplyr::select(year,
                         unitid,
                         instnm,
-                        unrestricted_net_assets =f2a04,
+                        expendable_net_assets = f2i05,
                         total_expenses = f2b02,
-                        change_in_net_assets = f2b04,
-                        total_revenues_and_investment_return = f2b01,
-                        net_assets_end_of_year = f2b07,
-                        total_assets = f2a02,
-                        debt_property_plant_equipment = f2a03a) %>%
-          dplyr::mutate(primary_reserve_ratio = round(unrestricted_net_assets / total_expenses, 2),
-                        net_income_ratio = round(change_in_net_assets / total_revenues_and_investment_return, 2),
-                        net_operating_revenue_ratio = round(net_assets_end_of_year / total_assets, 2),
-                        viability_ratio = round(unrestricted_net_assets / debt_property_plant_equipment, 2)) %>%
+                        plant_related_debt = f2i06,
+                        change_in_net_assets = f2i03,
+                        total_net_assets = f2i04) %>%
+          dplyr::mutate(primary_reserve_ratio = round(expendable_net_assets / total_expenses, 2),
+                        viability_ratio = round(expendable_net_assets / plant_related_debt, 2),
+                        return_on_net_assets_ratio = round(change_in_net_assets / total_net_assets, 2)) %>%
           dplyr::select(-unitid,
                         -instnm)
       }
@@ -327,7 +320,7 @@ server <- function(input, output, session) {
       else if (input$this_type == "Not-for-Profit") {
         df_financial() %>% 
           dplyr::select(year,
-                        unrestricted_net_assets,
+                        expendable_net_assets,
                         total_expenses,
                         primary_reserve_ratio) %>% 
           tidyr::pivot_longer(names_to = "column",
@@ -348,13 +341,13 @@ server <- function(input, output, session) {
           ) %>%
           fmt_number(
             columns = starts_with("2"),
-            rows = c("unrestricted_net_assets",
+            rows = c("expendable_net_assets",
                      "total_expenses"),
             decimals = 0,
             use_seps = TRUE
           ) %>%
           fmt_currency(columns = everything(),
-                       rows = c("unrestricted_net_assets",
+                       rows = c("expendable_net_assets",
                                 "total_expenses"),
                        decimals = 0) %>%
           data_color(
@@ -363,6 +356,96 @@ server <- function(input, output, session) {
             palette = "#A50021"
           )}
     })
+    
+    
+      # VIABILITY TABLE --------------------------------------------------------------------
+      output$tbl_viability <- render_gt({
+        if (input$this_type == "Public") {
+          df_financial() %>%
+            dplyr::select(year,
+                          expendable_net_assets,
+                          plant_related_debt,
+                          viability_ratio) %>%
+            tidyr::pivot_longer(names_to = "column",
+                                values_to = "values",
+                                cols = -c("year"),
+                                values_transform = list(values = as.character)) %>%
+            dplyr::mutate(values = as.numeric(values)) %>%
+            dplyr::arrange(desc(year)) %>%
+            tidyr::pivot_wider(names_from = "year",
+                               values_from = "values") %>%
+            gt(
+              rowname_col = "column"
+            ) %>%
+            tab_header(title = input$this_college) %>%
+            tab_spanner(
+              label = "Year",
+              columns = sort(input$this_year, decreasing = T)
+            ) %>%
+            fmt_number(
+              columns = starts_with("2"),
+              rows = c("expendable_net_assets",
+                       "plant_related_debt"),
+              decimals = 0,
+              use_seps = TRUE
+            ) %>%
+            fmt_currency(columns = everything(),
+                         rows = c("expendable_net_assets",
+                                  "plant_related_debt"),
+                         decimals = 0) %>%
+            data_color(
+              columns = everything(),
+              method = "numeric",
+              rows = "viability_ratio",
+              palette = "#FF7F00"
+            )}
+        else if (input$this_type == "Private") {
+
+        }
+        else if (input$this_type == "Not-for-Profit") {
+          df_financial() %>%
+            dplyr::select(year,
+                          expendable_net_assets,
+                          plant_related_debt,
+                          viability_ratio) %>%
+            tidyr::pivot_longer(names_to = "column",
+                                values_to = "values",
+                                cols = -c("year"),
+                                values_transform = list(values = as.character)) %>%
+            dplyr::mutate(values = as.numeric(values)) %>%
+            dplyr::arrange(desc(year)) %>%
+            tidyr::pivot_wider(names_from = "year",
+                               values_from = "values") %>%
+            gt(
+              rowname_col = "column"
+            ) %>%
+            tab_header(title = input$this_college) %>%
+            tab_spanner(
+              label = "Year",
+              columns = sort(input$this_year, decreasing = T)
+            ) %>%
+            fmt_number(
+              columns = starts_with("2"),
+              rows = c("expendable_net_assets",
+                       "plant_related_debt"),
+              decimals = 0,
+              use_seps = TRUE
+            ) %>%
+            fmt_currency(columns = everything(),
+                         rows = c("expendable_net_assets",
+                                  "plant_related_debt"),
+                         decimals = 0) %>%
+            data_color(
+              columns = everything(),
+              method = "numeric",
+              rows = "viability_ratio",
+              palette = "#FF7F00"
+            )
+      }
+    }) # close render gt
+    
+    
+  
     
   #   # NET ASSETS TABLE --------------------------------------------------------------------
   #   output$tbl_net_assets <- render_gt({
@@ -534,91 +617,7 @@ server <- function(input, output, session) {
   #         )}
   #   })
   #   
-  #   # VIABILITY TABLE --------------------------------------------------------------------
-  #   output$tbl_viability <- render_gt({
-  #     if (input$this_type == "Public") {
-  #       df_financial() %>% 
-  #         dplyr::select(year,
-  #                       expendable_net_assets,
-  #                       long_term_debt,
-  #                       viability_ratio) %>% 
-  #         tidyr::pivot_longer(names_to = "column",
-  #                             values_to = "values",
-  #                             cols = -c("year"),
-  #                             values_transform = list(values = as.character)) %>% 
-  #         dplyr::mutate(values = as.numeric(values)) %>% 
-  #         dplyr::arrange(desc(year)) %>% 
-  #         tidyr::pivot_wider(names_from = "year",
-  #                            values_from = "values") %>% 
-  #         gt(
-  #           rowname_col = "column"
-  #         ) %>% 
-  #         tab_header(title = input$this_college) %>% 
-  #         tab_spanner(
-  #           label = "Year",
-  #           columns = sort(input$this_year, decreasing = T)
-  #         ) %>%
-  #         fmt_number(
-  #           columns = starts_with("2"),
-  #           rows = c("expendable_net_assets",
-  #                    "long_term_debt"),
-  #           decimals = 0,
-  #           use_seps = TRUE
-  #         ) %>% 
-  #         fmt_currency(columns = everything(),
-  #                      rows = c("expendable_net_assets",
-  #                               "long_term_debt"),
-  #                      decimals = 0) %>% 
-  #         data_color(
-  #           columns = everything(),
-  #           method = "numeric",
-  #           rows = "viability_ratio",
-  #           palette = "#FF7F00"
-  #         )}
-  #     else if (input$this_type == "Private") {
-  #       
-  #     }
-  #     else if (input$this_type == "Not-for-Profit") {
-  #       df_financial() %>% 
-  #         dplyr::select(year,
-  #                       unrestricted_net_assets,
-  #                       debt_property_plant_equipment,
-  #                       viability_ratio) %>% 
-  #         tidyr::pivot_longer(names_to = "column",
-  #                             values_to = "values",
-  #                             cols = -c("year"),
-  #                             values_transform = list(values = as.character)) %>% 
-  #         dplyr::mutate(values = as.numeric(values)) %>% 
-  #         dplyr::arrange(desc(year)) %>% 
-  #         tidyr::pivot_wider(names_from = "year",
-  #                            values_from = "values") %>% 
-  #         gt(
-  #           rowname_col = "column"
-  #         ) %>% 
-  #         tab_header(title = input$this_college) %>% 
-  #         tab_spanner(
-  #           label = "Year",
-  #           columns = sort(input$this_year, decreasing = T)
-  #         ) %>%
-  #         fmt_number(
-  #           columns = starts_with("2"),
-  #           rows = c("unrestricted_net_assets",
-  #                    "debt_property_plant_equipment"),
-  #           decimals = 0,
-  #           use_seps = TRUE
-  #         ) %>% 
-  #         fmt_currency(columns = everything(),
-  #                      rows = c("unrestricted_net_assets",
-  #                               "debt_property_plant_equipment"),
-  #                      decimals = 0) %>% 
-  #         data_color(
-  #           columns = everything(),
-  #           method = "numeric",
-  #           rows = "viability_ratio",
-  #           palette = "#FF7F00"
-  #         )
-  #   }
-  # }) # close render gt
+  
   
   
 } # close server
